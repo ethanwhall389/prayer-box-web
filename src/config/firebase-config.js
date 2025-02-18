ea60d7416309389156cf670cd08328a8bd8536b1
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2025-02-18 19:50:30 +0000

    Add firebase-config, install firebase-tools

---
+++ src/config/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCYyPjQ0tb-DKu7mxCbKJWZPrsWBR_AL9s",
+  authDomain: "prayer-box-7f78c.firebaseapp.com",
+  projectId: "prayer-box-7f78c",
+  storageBucket: "prayer-box-7f78c.firebasestorage.app",
+  messagingSenderId: "46295454184",
+  appId: "1:46295454184:web:4e3aa317d79c6e6fa5145d",
+  measurementId: "G-MYHKCHGZLJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
