bf65a54e79a3fbc6fdb9aa2e8ddb963be177de4f
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2025-02-18 21:38:09 +0000

    Add basic styling to dashboard

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,30 @@
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+
 export const ButtonPrimary = ({text, onClick}) => {
     return (
-        <button className="p-2 px-4 bg-slate-400 rounded-3xl hover:cursor-pointer" 
+        <button className="p-2 px-4 bg-slate-400 rounded-3xl hover:cursor-pointer max-w-80" 
         onClick={onClick}>{text}</button>
     )
 }
 export const ButtonSecondary = ({text, onClick}) => {
     return (
-        <button className="p-2 px-4 bg-green-400 rounded-3xl hover:cursor-pointer" 
+        <button className="p-2 px-4 bg-green-400 rounded-3xl hover:cursor-pointer max-w-80" 
         onClick={onClick}>{text}</button>
+    )
+}
+export const ButtonCard = ({text, onClick}) => {
+    return (
+        <div className="h-28 w-full p-2 px-4 bg-blue-100 rounded-xl hover:cursor-pointer text-left flex items-center justify-between hover:bg-blue-200 transition-all" 
+        onClick={onClick}>
+            <p className="text-lg font-medium">{text}</p>
+            <ArrowForwardIcon />
+        </div>
+    )
+}
+export const ButtonIcon = ({icon, size, onClick}) => {
+    return (
+        <>
+            <div>{icon}</div>
+        </>
     )
 }
--- src/pages/dashboard/dashboard-index.jsx
+++ src/pages/dashboard/dashboard-index.jsx
@@ -1,7 +1,21 @@
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import { ButtonPrimary, ButtonSecondary, ButtonCard, ButtonIcon } from "../../components/Button"
+import { colors } from '@mui/material';
+
 export default function Dashboard() {
     return (
-        <>
-        <h1>Hello from dashboard!</h1>
-        </>
+        <div className="h-screen p-8 flex flex-col items-center gap-10">
+            <div className='flex justify-between w-full'>
+                <h1 className='text-xl font-bold'>Welcome, [name]!</h1>
+                <p>January 1st, 2025</p>
+            </div>
+            <p>Verse pertaining to prayer...</p>
+            <p>25 prayer requests</p>
+            <div className="w-full px-10 max-w-[850px] flex flex-col gap-5 justify-center items-center">
+                <ButtonCard text={"View today's prayer list"}/>
+                <ButtonCard text={"View your entire prayer list"}/>
+                <ButtonIcon icon={<SettingsOutlinedIcon style={{fontSize: '48px'}}/>} />
+            </div>
+        </div>
     )
 }
