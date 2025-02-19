da33985a666ac30d3ac65d46e0bf0d5fd8c840c4
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2025-02-19 20:53:34 +0000

    Update settings icon placement and style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,24 @@
 import { useGetUserInfo } from "../hooks/useGetUserInfo"
 import { useFormatDate } from "../hooks/useFormatDate";
+import { useNavigate } from "react-router-dom";
+import { ButtonIcon } from "./Button";
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export default function Header() {
 
     const {formatDate} = useFormatDate();
     const {firstName} = useGetUserInfo();
+    const navigate = useNavigate();
 
 
 
     return (
         <div className='flex justify-between w-full'>
             <h1 className='text-xl font-bold'>Welcome, {firstName}!</h1>
-            <p>{formatDate(new Date())}</p>
+            <div className="flex flex-col items-end gap-2">
+                <p>{formatDate(new Date())}</p>
+                <ButtonIcon icon={<SettingsIcon style={{fontSize: '35px'}}/>} onClick={() => navigate('/settings')} />
+            </div>
         </div>
     )
 }
--- src/pages/dashboard/dashboard-index.jsx
+++ src/pages/dashboard/dashboard-index.jsx
@@ -1,5 +1,4 @@
-import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
-import { ButtonCard, ButtonIcon } from "../../components/Button"
+import { ButtonCard} from "../../components/Button"
 import Header from '../../components/Header';
 
 import { useNavigate } from 'react-router-dom';
@@ -22,12 +21,11 @@ export default function Dashboard() {
     return (
         <div className="h-screen p-8 flex flex-col items-center gap-10">
             <Header />
-            <p>Verse pertaining to prayer...</p>
-            <p>25 prayer requests</p>
-            <div className="w-full px-10 max-w-[850px] flex flex-col gap-5 justify-center items-center">
+            <div className="w-full px-10 max-w-[850px] flex flex-col gap-5 justify-center items-start">
+                <p>Verse pertaining to prayer...</p>
+                <p>25 prayer requests</p>
                 <ButtonCard text={"View today's prayer list"} onClick={() => navigate('/list-today')}/>
                 <ButtonCard text={"View and edit your entire prayer list"} onClick={() => navigate('/list-entire')}/>
-                <ButtonIcon icon={<SettingsOutlinedIcon style={{fontSize: '48px'}}/>} onClick={() => navigate('/settings')} />
             </div>
         </div>
     )
