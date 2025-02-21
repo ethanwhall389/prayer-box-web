28c647cd5e0593e9f56f0bef89123d71ba53c6a1
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2025-02-21 17:25:13 +0000

    Change card displays to text inputs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,26 +2,41 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { ButtonIcon } from './Button';
 import useDeleteCard from '../hooks/useDeleteCard';
 import { useFormatDate } from '../hooks/useFormatDate';
+import { useState } from 'react';
 
 export default function Card({cardInfo, categoryName, boxData, setBoxData}) {
 
     const {deleteCard} = useDeleteCard(boxData, setBoxData)
     const {formatDate} = useFormatDate();
 
+    const [nameInput, setNameInput] = useState(cardInfo.cardTitle);
+    const [descriptionInput, setDescriptionInput] = useState(cardInfo.cardDescription);
+
     const cardCreatedAt = formatDate(cardInfo.createdAt);
+
+    function handleSubmit() {
+        //update state / update database
+        //updateCard(categoryName, nameInput, descriptionInput);
+    }
 
     return (
         <div className="bg-slate-100 w-full p-4 flex flex-col items-start group hover:cursor-pointer rounded-lg">
-            <div className="flex w-full justify-between">
-                <p className="text-lg">{cardInfo.cardTitle}</p>
-                <div className='group-hover:opacity-100 opacity-0 transition-all'>
-                    <ButtonIcon icon={<DeleteIcon />} rotate={false} onClick={() => deleteCard(categoryName, cardInfo.cardTitle)}/>
+            <form action="" className='w-full h-full flex flex-col gap-2'>
+                <div className="flex w-full justify-between">
+                    <input type="text" id='name' value={nameInput} onChange={(e) => setNameInput(e.target.value)} className='outline-none w-full text-lg font-bold'/>
+                    {/* <p className="text-lg">{cardInfo.cardTitle}</p> */}
+                    <div className='group-hover:opacity-100 opacity-0 transition-all'>
+                        <ButtonIcon icon={<DeleteIcon />} rotate={false} onClick={() => deleteCard(categoryName, cardInfo.cardTitle)}/>
+                    </div>
                 </div>
-            </div>
-            <p>{cardInfo.cardDescription}</p>
-            <div className='flex w-full justify-end'>
-                <p className='group-hover:opacity-100 opacity-0 transition-all'>{cardCreatedAt}</p>
-            </div>
+                <textarea name="description" id="description" value={descriptionInput} onChange={(e) => setDescriptionInput(e.target.value)} className='p-2 w-full resize-none'></textarea>
+                {/* <input type="text" id='name' value={descriptionInput} onChange={(e) => setDescriptionInput(e.target.value)} className='outline-none border-2 w-full'/> */}
+                {/* <p>{cardInfo.cardDescription}</p> */}
+                <div className='flex w-full justify-end'>
+                    <p className='group-hover:opacity-100 opacity-0 transition-all'>{cardCreatedAt}</p>
+                </div>
+                <button type='submit' onSubmit={handleSubmit} className='hidden'></button>
+            </form>
         </div>
     )
 }
