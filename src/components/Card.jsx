9b19b82b180cd7c781dba669fd6ebe7b370953e4
ethanwhall389 <125170834+ethanwhall389@users.noreply.github.com>
2025-02-24 20:13:55 +0000

    Add onBlur reset to inputs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -32,21 +32,25 @@ export default function Card({cardInfo, categoryName, boxData, setBoxData, messa
               el.blur(); // Removes focus from input fields
             });
         }
+    }
 
+    function handleBlur() {
+        setNameInput(cardInfo.cardTitle);
+        setDescriptionInput(cardInfo.cardDescription);
     }
 
     return (
         <div className="bg-slate-100 w-full p-4 flex flex-col items-start group hover:cursor-pointer rounded-lg">
             <form ref={formRef} onSubmit={(e) => handleSubmit(e)} action="" className='w-full h-full flex flex-col gap-2'>
                 <div className="flex w-full justify-between">
-                    <input type="text" id='name' value={nameInput} onChange={(e) => setNameInput(e.target.value)} className='outline-none w-full text-lg font-bold focus:bg-slate-200 p-2 px-3 mr-4 rounded-lg'/>
+                    <input type="text" id='name' value={nameInput} onBlur={handleBlur} onChange={(e) => setNameInput(e.target.value)} className='outline-none w-full text-lg font-bold focus:bg-slate-200 p-2 px-3 mr-4 rounded-lg'/>
                     {/* <p className="text-lg">{cardInfo.cardTitle}</p> */}
                     <div className='group-hover:opacity-100 opacity-0 transition-all'>
                         <ButtonIcon icon={<DeleteIcon />} rotate={false} onClick={() => deleteCard(categoryName, cardInfo.cardTitle)}/>
                     </div>
                 </div>
                 {descriptionInput !== '' &&
-                    <textarea name="description" id="description" value={descriptionInput} onChange={(e) => setDescriptionInput(e.target.value)} className='outline-none p-2 w-full resize-none rounded-lg focus:bg-slate-200'></textarea>
+                    <textarea name="description" id="description" value={descriptionInput} onBlur={handleBlur} onChange={(e) => setDescriptionInput(e.target.value)} className='outline-none p-2 w-full resize-none rounded-lg focus:bg-slate-200'></textarea>
                 }
                 {/* <input type="text" id='name' value={descriptionInput} onChange={(e) => setDescriptionInput(e.target.value)} className='outline-none border-2 w-full'/> */}
                 {/* <p>{cardInfo.cardDescription}</p> */}
