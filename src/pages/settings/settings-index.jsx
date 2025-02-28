7cbd5d49ae555e19991d41b14f87feeec74a5aa9
ethanwhall389 <ethanwhall@icloud.com>
2025-02-28 08:32:27 -0500

    feat(settings) style and layout

--- src/pages/settings/settings-index.jsx
+++ src/pages/settings/settings-index.jsx
@@ -11,6 +11,9 @@ import { useGetUserInfo } from "../../hooks/useGetUserInfo"
 import { doc } from "firebase/firestore"
 import { useEffect, useState } from "react"
 import Modal from "../../components/Modal"
+import PersonIcon from '@mui/icons-material/Person';
+import BrushIcon from '@mui/icons-material/Brush';
+import InboxIcon from '@mui/icons-material/Inbox';
 
 export default function Settings({isAuth}) {
 
@@ -26,7 +29,7 @@ export default function Settings({isAuth}) {
         return null;
     }
     
-    const {userID} = useGetUserInfo();
+    const {userID, displayName} = useGetUserInfo();
     const user = auth.currentUser;
     const boxDocRef = doc(db, "boxes", userID);
     const [modalOpen, setModalOpen] = useState(false);
@@ -66,9 +69,42 @@ export default function Settings({isAuth}) {
                     </div>
                 </Modal>
             }
-            <div className="w-full h-full py-20 px-10 max-w-[850px] flex flex-col gap-5 justify-start items-start">
-                <ButtonPrimary text={'Sign Out'} onClick={signUserOut}/>
-                <ButtonDanger text={'Delete My Account'} onClick={() => setModalOpen(true)}/>
+            <div className="w-full h-full py-20 px-10 max-w-[850px] flex flex-col gap-8 justify-start items-start">
+                <div className="w-full flex flex-col items-start gap-4">
+                    <h1 className="text-2xl font-bold">{displayName}</h1>
+                    <p>personal profile</p>
+                </div>
+
+                <div className="w-full flex flex-col gap-10">
+                    <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
+                        <InboxIcon />
+                        <h2 className="text-xl">Prayer Box Preferences</h2>
+                    </div>
+                    <div className="w-full flex flex-col items-start gap-2">
+                        {/* Appearance options here */}
+                    </div>
+                </div>
+
+                <div className="w-full flex flex-col gap-10">
+                    <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
+                        <BrushIcon />
+                        <h2 className="text-xl">Appearance</h2>
+                    </div>
+                    <div className="w-full flex flex-col items-start gap-2">
+                        {/* Appearance options here */}
+                    </div>
+                </div>
+
+                <div className="w-full flex flex-col gap-10">
+                    <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
+                        <PersonIcon />
+                        <h2 className="text-xl">Account Settings</h2>
+                    </div>
+                    <div className="w-full flex flex-col items-start gap-2">
+                        <ButtonPrimary text={'Sign Out'} onClick={signUserOut}/>
+                        <ButtonDanger text={'Delete My Account'} onClick={() => setModalOpen(true)}/>
+                    </div>
+                </div>
             </div>
         </div>
     )
