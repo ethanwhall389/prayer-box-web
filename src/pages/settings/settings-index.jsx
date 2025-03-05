e23640e075b57d07b573276dc866341effe9c7b8
Ethan Hall <ethanwhall@icloud.com>
2025-03-05 15:53:18 -0500

    feat(mobile) add back button to settings page

--- src/pages/settings/settings-index.jsx
+++ src/pages/settings/settings-index.jsx
@@ -16,6 +16,7 @@ import BrushIcon from '@mui/icons-material/Brush';
 import InboxIcon from '@mui/icons-material/Inbox';
 import InfoIcon from '@mui/icons-material/Info';
 import AnimatedPage from "../../components/AnimatedPage"
+import BackArrow from "../../components/BackArrow"
 
 export default function Settings() {
 
@@ -73,51 +74,54 @@ export default function Settings() {
                     </div>
                 </Modal>
             }
-            <div className="w-full h-full py-20 px-10 max-w-[850px] flex flex-col gap-8 justify-start items-start">
-                <div className="w-full flex flex-col items-start gap-4">
-                    <h1 className="text-2xl font-bold">{displayName}</h1>
-                    <p>personal profile</p>
-                </div>
+            <div className="flex flex-col w-full items-center">
+                <BackArrow />
+                <div className="w-full h-full py-12 px-10 max-w-[850px] flex flex-col gap-8 justify-start items-start">
+                    <div className="w-full flex flex-col items-start gap-4">
+                        <h1 className="text-2xl font-bold">{displayName}</h1>
+                        <p>personal profile</p>
+                    </div>
 
-                <div className="w-full flex flex-col gap-10">
-                    <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
-                        <InboxIcon />
-                        <h2 className="text-xl">Prayer Box Preferences</h2>
+                    <div className="w-full flex flex-col gap-10">
+                        <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
+                            <InboxIcon />
+                            <h2 className="text-xl">Prayer Box Preferences</h2>
+                        </div>
+                        <div className="w-full flex flex-col items-start gap-2">
+                            {/* Preference options here */}
+                        </div>
                     </div>
-                    <div className="w-full flex flex-col items-start gap-2">
-                        {/* Preference options here */}
-                    </div>
-                </div>
 
-                <div className="w-full flex flex-col gap-10">
-                    <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
-                        <BrushIcon />
-                        <h2 className="text-xl">Appearance</h2>
+                    <div className="w-full flex flex-col gap-10">
+                        <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
+                            <BrushIcon />
+                            <h2 className="text-xl">Appearance</h2>
+                        </div>
+                        <div className="w-full flex flex-col items-start gap-2">
+                            {/* Appearance options here */}
+                        </div>
                     </div>
-                    <div className="w-full flex flex-col items-start gap-2">
-                        {/* Appearance options here */}
-                    </div>
-                </div>
 
-                <div className="w-full flex flex-col gap-10">
-                    <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
-                        <PersonIcon />
-                        <h2 className="text-xl">Account Settings</h2>
+                    <div className="w-full flex flex-col gap-10">
+                        <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
+                            <PersonIcon />
+                            <h2 className="text-xl">Account Settings</h2>
+                        </div>
+                        <div className="w-full flex flex-col items-start gap-2">
+                            <ButtonPrimary text={'Sign Out'} onClick={signUserOut}/>
+                            <ButtonDanger text={'Delete My Account'} onClick={() => setModalOpen(true)}/>
+                        </div>
                     </div>
-                    <div className="w-full flex flex-col items-start gap-2">
-                        <ButtonPrimary text={'Sign Out'} onClick={signUserOut}/>
-                        <ButtonDanger text={'Delete My Account'} onClick={() => setModalOpen(true)}/>
-                    </div>
-                </div>
-                
-                <div className="w-full flex flex-col gap-10">
-                    <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
-                        <InfoIcon />
-                        <h2 className="text-xl">About Prayer Box</h2>
-                    </div>
-                    <div className="w-full flex flex-col items-start gap-2">
-                        <p>Prayer Box v. 1.0.0</p>
-                        <p>Built and maintained by <a href="https://github.com/ethanwhall389" target="_new">Ethan Hall</a></p>
+                    
+                    <div className="w-full flex flex-col gap-10">
+                        <div className="w-full border-b-1 border-slate-500 py-8 flex justify-start items-center gap-4">
+                            <InfoIcon />
+                            <h2 className="text-xl">About Prayer Box</h2>
+                        </div>
+                        <div className="w-full flex flex-col items-start gap-2">
+                            <p>Prayer Box v. 1.0.0</p>
+                            <p>Built and maintained by <a href="https://github.com/ethanwhall389" target="_new">Ethan Hall</a></p>
+                        </div>
                     </div>
                 </div>
             </div>
